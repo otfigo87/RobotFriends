7b04258b69351655f364c0ad584b765b646a5537
Adam Otmane Aatik <56803295+otfigo87@users.noreply.github.com>
2020-12-05 15:14:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CardList from './components/CardList'
+import SearchBox from './components/SearchBox'
+import Scroll from './components/Scroll'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+    componentDidMount(){
+        fetch('https:jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return(
+                <div className="tc">
+                    <h1 className="h1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                    <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            )
+        }
+        
+    }
+    
 }
 
-export default App;
+export default App
---
+++ src/components/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Scroll(props){
+    return(
+        <div style={{overflowY: 'scroll', height: '600px'}}>
+            {props.children}
+        </div>
+    )
+}
+
+
+
+
+export default Scroll
