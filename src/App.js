9a5c53b4058a4589a855de12e1886865b2541a7f
Adam Otmane Aatik <56803295+otfigo87@users.noreply.github.com>
2020-12-16 19:33:40 -0500

    using hooks instead of class component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CardList from './components/CardList'
 import SearchBox from './components/SearchBox'
 import Scroll from './components/Scroll'
 
 
-class App extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
-    componentDidMount(){
-        fetch('https:jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}));
-    }
+const App = () => {
+    
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    
+    
+   useEffect( () => {
+    fetch('https:jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => setRobots(users));
+   }, [])
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
         
     }
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
+        if (robots.length === 0) {
             return <h1>Loading...</h1>
         } else {
             return(
                 <div className="tc">
                     <h1 className="h1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                     <CardList robots={filteredRobots} />
                     </Scroll>
@@ -42,6 +40,7 @@ class App extends React.Component {
         
     }
     
-}
+
+
 
 export default App
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Scroll(props){
     return(
-        <div style={{overflowY: 'scroll', height: '600px'}}>
+        <div style={{overflowY: 'scroll', height: '700px'}}>
             {props.children}
         </div>
     )
